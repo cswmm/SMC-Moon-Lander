e788b926503a704facfe997a34ce9241e6d5410b
Nathan Durrant <nathandurrant726@gmail.com>
2025-12-04 13:06:27 -0800

    Added thruster to player

--- src/ParticleEmitter.cpp
+++ src/ParticleEmitter.cpp
@@ -27,8 +27,8 @@ ParticleEmitter::~ParticleEmitter() {
 }
 
 void ParticleEmitter::init() {
-	rate = 1;
-	velocity = ofVec3f(0, 20, 0);
+	rate = 20;
+	velocity = ofVec3f(0, -5, 0);
 	lifespan = 3;
 	started = false;
 	lastSpawned = 0;
@@ -69,34 +69,38 @@ void ParticleEmitter::update() {
 
 	if ((time - lastSpawned) > (1000.0 / rate)) {
 
-		// spawn a new particle
-		//
-		Particle particle;
+		if (active) {
+			// spawn a new particle
+			//
+			Particle particle;
 
-		// set initial velocity and position
-		// based on emitter type
-		//
-		switch (type) {
-		case RadialEmitter:
-			//		break;
-		case SphereEmitter:
-			//		break;
-		case DirectionalEmitter:
-			particle.velocity = velocity;
-			particle.position.set(position);
-			break;
+			// set initial velocity and position
+			// based on emitter type
+			//
+			switch (type) {
+			case RadialEmitter:
+				//		break;
+			case SphereEmitter:
+				//		break;
+			case DirectionalEmitter:
+				particle.velocity = velocity;
+				particle.position.set(position);
+				break;
+			}
+
+			// other particle attributes
+			//
+			particle.lifespan = lifespan;
+			particle.birthtime = time;
+			particle.radius = particleRadius;
+
+			// add to system
+			//
+			sys->add(particle);
+			lastSpawned = time;
+
 		}
 
-		// other particle attributes
-		//
-		particle.lifespan = lifespan;
-		particle.birthtime = time;
-		particle.radius = particleRadius;
-
-		// add to system
-		//
-		sys->add(particle);
-		lastSpawned = time;
 	}
 	sys->update();
 }
